3db6ac6e217f052f8270eccc86d45670058316fb
Sanjeet kumar <sanjeetkumar867496@gmail.com>
2025-03-13 21:09:15 +0530

    sanjeet

---
+++ Taking-input.py
@@ -0,0 +1,6 @@
+# Taking input from User & printing it.
+name = input("name : ")
+age = int(input("age : "))
+price = float(input("price :"))
+
+print("my name is" , name ,"and i am" , age,"year old")
